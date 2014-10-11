47a09bfdd63bed8151c061e120a5c1043f5344f8
Tomasz Pewiński <pewniak747@gmail.com>
2014-10-11 15:06:49 +0200

    Initial commit

---
+++ listen.py
@@ -0,0 +1,76 @@
+import pyaudio
+import numpy as np
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+import sys
+import time
+
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
+RATE = 44100
+BYTE_DURATION = 0.1
+CHUNKS_PER_BYTE = 4
+CHUNK = int(RATE * BYTE_DURATION / CHUNKS_PER_BYTE)
+
+def tone(i):
+  return (2 ** (i / 12)) * 440
+
+SYNC_FQ = int(tone(38.0))
+FREQUENCIES = [int(tone(30.0 + x)) for x in range(0, 8)]
+THRESHOLD = 30
+NOISE_THRESHOLD = 10
+GRAPH = False
+
+print "sample rate: {0} Hz".format(RATE)
+print "chunk size: {0}".format(CHUNK)
+print "carrier fq's: {0}".format(FREQUENCIES)
+print "sync fq: {0}".format(SYNC_FQ)
+
+pa = pyaudio.PyAudio()
+stream = pa.open(format=FORMAT,
+  channels=CHANNELS,
+  rate=RATE,
+  input=True,
+  frames_per_buffer=CHUNK)
+
+if GRAPH:
+  min_fq = min([ SYNC_FQ, min(FREQUENCIES) ]) - 100
+  max_fq = max([ SYNC_FQ, max(FREQUENCIES) ]) + 100
+  plt.ion()
+  fft_graph = plt.plot([])[0]
+  plt.ylim([0, 100])
+  plt.xlim([min_fq, max_fq])
+  for fq in FREQUENCIES:
+    plt.axvline(fq, 0, 100, color='gray')
+  plt.axvline(SYNC_FQ, 0, 100, color='r')
+  plt.axhline(THRESHOLD, 0, max_fq, color='gray')
+  plt.draw()
+  plt.show()
+
+window = signal.gaussian(3, 10, False)
+
+while True:
+  try:
+    block = stream.read(CHUNK)
+    decoded = np.fromstring(block, 'Float32');
+    raw_fft = np.abs(np.fft.rfft(decoded))
+    filtered = np.array(map(lambda x: x if x > NOISE_THRESHOLD else 0.0, raw_fft))
+    filtered = signal.fftconvolve(window, filtered)
+    filtered = (np.average(raw_fft) / np.average(filtered)) * filtered
+
+    fft = signal.resample(raw_fft, RATE/2)
+
+    sync = True if fft[int(SYNC_FQ)] > THRESHOLD else False
+    bts = map(lambda x: 1 if fft[int(x)] > THRESHOLD else 0, FREQUENCIES)
+    byte = np.sum([ 2 ** idx if bit else 0 for idx, bit in enumerate(bts) ])
+    char = chr(byte) if 32 <= byte <=126 else '.'
+    #print "{0}\t{1}\t{2}".format(byte, char, sync)
+    if sync:
+      sys.stdout.write(char)
+      sys.stdout.flush()
+
+    if GRAPH:
+      fft_graph.set_data(range(0, len(fft)), fft)
+      plt.draw()
+  except IOError:
+    continue
---
+++ sound.rb
@@ -0,0 +1,71 @@
+require 'coreaudio'
+
+payload = if ARGV[0] then ARGV[0] else "Hello World" end.encode("ascii")
+bytes = payload.unpack("C*")
+
+def tone(i)
+  (2 ** (i.to_f / 12)) * 440
+end
+
+SYNC_FQ = tone(38).to_i
+FREQUENCIES = Array.new(8) { |i| tone(30 + i).to_i }
+
+puts "payload:\t#{payload}"
+puts "bytes:\t\t#{bytes}"
+puts "fqs:\t\t#{FREQUENCIES}"
+puts "sync fq:\t#{SYNC_FQ}"
+
+dev = CoreAudio.default_output_device
+byte_duration = 0.1
+sync_duration = byte_duration / 4
+SAMPLE_RATE = dev.nominal_rate
+BUFFER_SIZE = (byte_duration * SAMPLE_RATE).to_i
+SYNC_SIZE = (sync_duration * SAMPLE_RATE).to_i
+BUFFER = dev.output_buffer(BUFFER_SIZE)
+
+BUFFER.start
+
+def play_text(text)
+  play_bytes(text.unpack('C*'))
+end
+
+def play_bytes(bytes)
+  sync_signal = Array.new(SYNC_SIZE) { |i| Math.sin((2 * i * Math::PI / SAMPLE_RATE) * SYNC_FQ) }
+  wave = NArray.sint(BUFFER_SIZE)
+
+  bytes.each do |byte|
+
+    mods = 8.times.map do |i|
+      0 != (byte & 0b01 << i)
+    end
+
+    samples = BUFFER_SIZE.times.map do |i|
+      sines = FREQUENCIES.zip(mods).map do |f, m|
+        sine = Math.sin((2 * i * Math::PI / SAMPLE_RATE) * f)
+        if m
+          sine
+        else
+          0.0
+        end
+      end
+      sines << sync_signal[i] if i < sync_signal.size
+      sines.reduce(:+) / (FREQUENCIES.size + 1)
+    end
+    samples.map.with_index do |sample, i|
+      wave[i] = sample * 32767
+    end
+    BUFFER << wave
+  end
+end
+
+bytes = [ 0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000 ]
+
+play_bytes(bytes)
+
+while text = $stdin.read(16) do
+  play_text(text)
+end
+
+sleep byte_duration
+
+BUFFER.stop
